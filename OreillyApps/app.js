a728996aff87bd1d977af04afff7b3f677857891
T. Simpson <thomas.simpson@gmail.com>
2020-09-09 18:16:15 -0500

    Update app.js

--- OreillyApps/app.js
+++ OreillyApps/app.js
@@ -1,13 +1,14 @@
 var main = function () {
-    "use strict"; //
+    "use strict"; // This allow for the js to be more flexible.
 
     $(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>");
+        var $new_comment = $("<p>"),
+        comment_text = $(".comment-input input").val();
 
+        $new_comment.text(comment_text);
 
-        $new_comment.text("this is a new comment");
         $(".comments").append($new_comment);
     });
 };
 
-$(document).ready(main);
+$(document).ready(main); //DOM call: Document > Main
