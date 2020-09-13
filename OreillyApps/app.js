fc4cc942e56e02181745f13cf64706b764156f7a
T. Simpson <thomas.simpson@gmail.com>
2020-09-13 09:26:15 -0500

    update

--- OreillyApps/app.js
+++ OreillyApps/app.js
@@ -2,10 +2,9 @@ var main = function () {
     "use strict";
 
     $(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>"),
-            comment_text = $(".comment-input input").val();
-            
-        $new_comment.text(comment_text);
+        
+        var $new_comment = $("<p>"), comment_text = $(".comment-input input").val();
+            $new_comment.text(comment_text);
             
         $(".comments").append($new_comment);
     });
