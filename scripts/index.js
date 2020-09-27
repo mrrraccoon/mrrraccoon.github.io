43a2ec1cd011ffaccc2245501b3026f6891019a0
T. Simpson <thomas.simpson@gmail.com>
2020-09-27 03:27:02 -0500

    Navbar Update
    Switching over the navbar code and included some potentially functional js. Fingers crossed.

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-var nav = function () {
-    "use strict";
-
-    document.getElementById("nav").style.flexDirection = "column";
-};
-
-$(document).ready(nav)
-
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+        x.className += "responsive";
+    } else {
+        x.className = "nav";
+    }
+}
